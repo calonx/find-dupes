f15669c200f7ce3b738ea38ddbbd9324bba89b59
Paul Marden <pmarden@gmail.com>
2019-02-10 15:40:32 -0500

    Checkpoint for find-dupes work

--- find_dupes.py
+++ find_dupes.py
@@ -2,6 +2,7 @@
 import hashlib
 import os
 import sys
+import multiprocessing as mp
 from tqdm import tqdm
 
 s_TargetPath = os.path.split(os.getcwd())[0]
@@ -9,13 +10,15 @@ s_TargetPath = os.path.split(os.getcwd())[0]
 s_Files = []
 
 #---------------------------------------------------------------------------------------------------
-def CountItems(file_path):
+def CountItems(file_path, callback=None):
     if not os.path.exists(file_path):
         return 0
     count = 1
+    if callback:
+        callback(file_path)
     if os.path.isdir(file_path):
         for entry in os.scandir(file_path):
-            count += CountItems(entry.path)
+            count += CountItems(entry.path, callback)
     return count
         
 
@@ -38,16 +41,30 @@ def ProcessItem(file_path, callback=lambda path, digest: print(f'{digest} {path}
     return m.digest()
 
 #---------------------------------------------------------------------------------------------------
+def ProcessTree(path):
+    print(f'Processing {path}...')
+
+    with tqdm('Counting', unit=' files') as prog:
+        count = CountItems(path, lambda p: prog.update(1))
+
+    # with tqdm('Hashing', total=count, unit=' files') as progress:
+    #     def callback(path, digest):
+    #         progress.update(1)
+
+    #     ProcessItem(path, callback)
+
+#---------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print('No path(s) provided.')
         exit(1)
 
-    with open('results.log', 'w') as f:
-        for path in sys.argv[1:]:
-            count = CountItems(path)
-            print(f'{count} {path}')
-            with tqdm(total=count, unit='files') as progress:
-                def callback(path, digest):
-                    progress.update(1)
-                ProcessItem(path, callback)
+    processes = [mp.Process(target=ProcessTree, args=[path]) for path in sys.argv[1:]]
+
+    for proc in processes:
+        proc.start()
+    
+    for proc in processes:
+        proc.join()
+    
+    print('All done!')
